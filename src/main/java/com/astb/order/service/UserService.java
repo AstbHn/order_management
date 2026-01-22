a9114d96e898f86a979764c491553e99f4e42c2a
AstbHn <astbhnd9@gmail.com>
2026-01-22 16:39:48 +0900

    feat: add user service

---
+++ src/main/java/com/astb/order/service/UserService.java
@@ -0,0 +1,41 @@
+package com.astb.order.service;
+
+import com.astb.order.domain.user.*;
+import com.astb.order.dto.SignupDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class UserService {
+
+    private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
+
+    //회원가입
+    public void signup(SignupDTO dto) {
+
+        User user = new User();
+        user.setUserId(dto.getUserId());
+        user.setPassword(passwordEncoder.encode(dto.getPassword()));
+        user.setAddress(dto.getAddress());
+        user.setRole(dto.getRole());
+
+        userMapper.insertUser(user);
+    }
+
+    @Transactional(readOnly = true)
+    public User findByUserId(String userId) {
+        return userMapper.findByUserId(userId);
+    }
+
+    // 아이디 중복 체크
+    @Transactional(readOnly = true)
+    public boolean existsByUserId(String userId) {
+        return userMapper.findByUserId(userId) != null;
+    }
+}
+
