e78d989051a40ad02f86371012586b8af4f352f1
AstbHn <astbhnd9@gmail.com>
2026-01-22 17:01:32 +0900

    feat: add user insert mapper

--- src/main/java/com/astb/order/domain/user/UserMapper.java
+++ src/main/java/com/astb/order/domain/user/UserMapper.java
@@ -1,5 +1,10 @@
 package com.astb.order.domain.user;
 
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface UserMapper {
     User findByUserId(String userId);
+    void insertUser(User user);
+
 }
