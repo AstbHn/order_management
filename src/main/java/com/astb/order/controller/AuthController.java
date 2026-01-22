e2792f935d9477c5e6191545491511f8b57c4568
AstbHn <astbhnd9@gmail.com>
2026-01-22 16:49:31 +0900

    feat: add auth controller

--- src/main/java/com/astb/order/controller/AuthController.java
+++ src/main/java/com/astb/order/controller/AuthController.java
@@ -1,11 +1,47 @@
 package com.astb.order.controller;
 
+import com.astb.order.dto.SignupDTO;
+import com.astb.order.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor // final 선언을 위한 어노테이션
 @RequestMapping("/auth")
 public class AuthController {
+    private final UserService userService;
     //비인증전용 컨트롤러
+    @GetMapping("/")
+    public String index(){
+        return "auth/index";
+    }
+    @GetMapping("/login")
+    public String loginPage() {
+        return "auth/login";
+    }
+    @GetMapping("/register")
+    public String register(){
+        return "auth/register";
+    }
+
+
+    @PostMapping("/signup")
+    public String signup(
+            @Valid SignupDTO dto,
+            BindingResult result
+    ) {
+        /* === 회원가입 시도 === */
+        if (result.hasErrors()) {
+            return "auth/register";
+        }
+
+        userService.signup(dto);
+        return "redirect:/auth/login";
+    }
 
 }
